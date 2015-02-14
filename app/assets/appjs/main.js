cf870381f8f63cc09c4d3e8f90d1f0c07facf794
Arshad Ansari <arshad.ansari@ensuantinc.com>
2015-02-14 22:14:12 +0530

    Login Box

--- app/assets/appjs/main.js
+++ app/assets/appjs/main.js
@@ -17,33 +17,45 @@ $(document).ready(function(){
                 return $message;
             },
             buttons: [{
+                id: 'btn-sign-in',
                 label: 'Sign In',
-                cssClass: 'btn-primary',
+                cssClass: 'btn-link',
                 action: function (dialogRef) {
                         var $message = $('<div></div>');
                         $message.load('/main/login');
                         dialogRef.setTitle("Sign In / Register");
                         dialogRef.getModalBody().html($message);
+                        $('#btn-sign-in').hide();
+                        $('#btn-subscribe').show();
+                        $('#btn-forgot-password').show();
                     }
                 },
                 {
+                id: 'btn-forgot-password',
                 label: 'Forgot Password',
-                cssClass: 'btn-primary',
+                cssClass: 'btn-link',
                 action: function (dialogRef) {
                         var $message = $('<div></div>');
                         $message.load('/main/forgot_password');
                         dialogRef.setTitle("Forgot Password");
                         dialogRef.getModalBody().html($message);
+                        $('#btn-sign-in').show();
+                        $('#btn-subscribe').show();
+                        $('#btn-forgot-password').hide();
                     }
                 },
                 {
+                id: 'btn-subscribe',
                 label: 'Subscribe',
-                cssClass: 'btn-success',
+                cssClass: 'btn-link',
                 action: function (dialogRef) {
                         var $message = $('<div></div>');
                         $message.load('/main/subscribe');
                         dialogRef.setTitle("Subscribe");
                         dialogRef.getModalBody().html($message);
+                        $('#btn-sign-in').show();
+                        $('#btn-forgot-password').hide();
+                        $('#btn-subscribe').hide();
                     }
                 },
                 {
