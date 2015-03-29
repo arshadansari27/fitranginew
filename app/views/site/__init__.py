5c3e008305a5b2ebdaecdee8b602516401ec4c4c
Arshad Ansari <arshad.ansari@ensuantinc.com>
2015-03-29 19:48:55 +0530

    Changes

--- app/views/site/__init__.py
+++ app/views/site/__init__.py
@@ -25,13 +25,29 @@ def activity_view():
 
 @app.route("/explore/adventure")
 def list_adventure():
-    view = NodeCollectionView("adventure", "grid", {})
-    return render_template('site/features/explore/adventures.html', view=view.get_card())
+    page = int(request.args.get('page', 1))
+    if page is 1:
+        view = NodeCollectionView("adventure", "grid", {}).get_card()
+        return render_template('site/features/explore/adventures.html', view=view)
+    else:
+        return NodeCollectionView("adventure", "grid", {}, page=page).next_page()
 
 @app.route("/community")
 def journal():
     return render_template("/site/features/community/index.html")
 
+@app.route("/listing/page")
+def paged_list():
+    card_type = request.args.get('card_type', 'detail')
+    model_view = request.args.get('model_view', None)
+    key = request.args.get('key', None)
+    value = request.args.get('value', None)
+    page = int(request.args.get('page', 1))
+    size = 10
+    filters = {}
+    if key and value:
+        filters[key] = value
+    return NodeCollectionView(model_name=model_view, card_type=card_type, filters=filters, paged=True, page=page, size=size).next_page(page)
 
 @app.route("/views/static")
 def static_model_views():
@@ -57,11 +73,12 @@ def template_views():
         key = request.args.get('key', 'pk')
         value = request.args.get('value', '')
         node_view = NodeView.factory(model_view)
-        view = node_view(card_type, value, key).get_card()
+        context=dict(card_type=card_type, id=value, key=key)
+        view = node_view(**context).get_card()
     else:
         key = request.args.get('key', None)
         value = request.args.get('value', None)
-        page = request.args.get('page', None)
+        page = int(request.args.get('page', 1))
         size = 10
         filters = {}
         if key and value:
@@ -70,6 +87,8 @@ def template_views():
             paged = True
         else:
             paged = False
-        view = NodeCollectionView(model_name=model_view, card_type=card_type, filters=filters, paged=paged, page=page, size=size).get_card()
 
-    return render_template("site/empty_layout.html", view=view)
+        context = dict(model_name=model_view, card_type=card_type, filters=filters, paged=paged, page=page, size=size)
+        view = NodeCollectionView(**context).get_card()
+
+    return render_template("site/empty_layout.html", view=view, **context)
