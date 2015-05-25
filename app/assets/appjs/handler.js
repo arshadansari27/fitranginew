6f6a0d2bcd306561dcf2c048e1227aa369d79dbe
Arshad Ansari <arshadansari27@gmail.com>
2015-05-26 01:14:15 +0530

    Fixed #105, Fixed #137

--- app/assets/appjs/handler.js
+++ app/assets/appjs/handler.js
@@ -1058,6 +1058,34 @@ jQuery(document).ready(function ($) {
      }
      */
 
+     $('.owl-carousel').owlCarousel({
+         loop:true,
+        margin:10,
+	    nav:false,
+	    autoplay:true,
+        autoplayTimeout:3000,
+	    autoplayHoverPause:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+			    loop:true,
+            },
+            767:{
+                items:1,
+                nav:false,
+			    loop:true,
+            },
+            1000:{
+                items:2,
+                nav:true,
+                loop:true,
+            }
+        }
+     }
+    });
+
      $('.hoverable').popover();
      setTimeout(function() { $('.alert').hide(); }, 10000);
 });
