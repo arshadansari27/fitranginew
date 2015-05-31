1347a05b564bdbe9f66899813545a3b718bce09a
Arshad Ansari <arshadansari27@gmail.com>
2015-05-31 14:35:44 +0530

    fixed #205

--- app/views/admin/__init__.py
+++ app/views/admin/__init__.py
@@ -226,7 +226,7 @@ class TripAdminView(ModelView):
 class ProfileAdminView(ModelView):
     create_template = 'admin/my_custom/create.html'
     edit_template = 'admin/my_custom/edit.html'
-    form_columns = ['name', 'email', 'featured', 'about', 'phone', 'website', 'facebook', 'twitter', 'google_plus', 'linked_in',  'youtube_channel', 'blog_channel', 'email_enabled', 'email_frequency', 'bookmarks', 'is_business_profile', 'roles', 'cover_image', 'type', 'managed_by']
+    form_columns = ['name', 'email', 'address', 'alternative_email', 'featured', 'about', 'phone', 'alternative_phone', 'website', 'facebook', 'twitter', 'google_plus', 'linked_in',  'youtube_channel', 'blog_channel', 'email_enabled', 'email_frequency', 'bookmarks', 'is_business_profile', 'roles', 'cover_image', 'type', 'managed_by', 'interest_in_activities']
     column_list = ('name', 'email', 'cover_image', 'user_since', 'last_login', 'type', 'featured', 'location')
     column_filters = ['name'] #, FilterProfileType('type.id', 'Type')]
     column_searchable_list = ('name', 'email')
