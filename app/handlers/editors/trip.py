537e0eba229d5b78a35fdfbd20ff98dfa248f43d
Arshad Ansari <arshadansari27@gmail.com>
2015-09-09 14:21:38 +0530

    Fixes

--- app/handlers/editors/trip.py
+++ app/handlers/editors/trip.py
@@ -137,9 +137,10 @@ def _edit(data, node=None):
     if data.get('location_name'):
         node.location = data['location_name']
         if data.get('location_lat') and data.get('location_lng'):
-            loc_lng = [float(data['location_lat']), float(data['location_lng'])]
-            print 'Lat/Long', loc_lng
-            node.geo_location = loc_lng  # [float(data['location_lat']), float(data['location_lng'])]
+            lat, lng = float(data['location_lat']), float(data['location_lng'])
+            point = {"type": "Point", "coordinates": [lat, lng]}
+            print 'Lat/Long', point
+            node.geo_location = point  # [float(data['location_lat']), float(data['location_lng'])]
         if data.get('location_city'):
             node.city = data['location_city']
         if data.get('location_region'):
